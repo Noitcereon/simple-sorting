361e653872382fc85a3db8ddb760fcf42e4a26ca
Thomas 'Noit' A <tba@live.dk>
2022-08-05 09:05:24 +0200

    Add my_item with a texture to the game

--- src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
+++ src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
@@ -1,6 +1,12 @@
 package com.noitcereon.simplesorting;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +15,10 @@ public class SimpleSortingMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("simplesorting");
+	public static final Item MY_ITEM = new Item(
+			new FabricItemSettings()
+					.group(ItemGroup.MISC)
+	);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +26,7 @@ public class SimpleSortingMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initialising simplesorting.");
+		Registry.register(Registry.ITEM, new Identifier("simplesorting", "my_item"), MY_ITEM);
 	}
 }
