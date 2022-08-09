1956582d83e392209a486c0aea944fb27ab46aea
Thomas 'Noit' A <tba@live.dk>
2022-08-09 08:58:46 +0200

    Fix: Make double chests sort full inventory

--- src/main/java/com/noitcereon/simplesorting/mixin/SortInventory.java
+++ src/main/java/com/noitcereon/simplesorting/mixin/SortInventory.java
@@ -1,16 +1,23 @@
 package com.noitcereon.simplesorting.mixin;
 
 import com.noitcereon.simplesorting.sorting.InventorySorter;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
+import net.minecraft.block.DoubleBlockProperties;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerFactory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,13 +28,14 @@ public abstract class SortInventory {
 
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     protected ActionResult sortChestOnInteraction(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> callbackInfo) {
-        if(world.isClient) return ActionResult.SUCCESS;
-        Inventory chestInventory = (Inventory) world.getBlockEntity(pos);
-        if(chestInventory == null) return ActionResult.PASS;
+        if (world.isClient) return ActionResult.SUCCESS;
+        Inventory chestInventory = ChestBlock.getInventory((ChestBlock) state.getBlock(), state, world, pos, false);
+
+        if (chestInventory == null) return ActionResult.PASS;
         chestInventory.markDirty();
 
         boolean isSorted = InventorySorter.sortInventory(chestInventory);
-        if(isSorted) return ActionResult.CONSUME_PARTIAL;
+        if (isSorted) return ActionResult.CONSUME_PARTIAL;
         else return ActionResult.PASS;
     }
 
