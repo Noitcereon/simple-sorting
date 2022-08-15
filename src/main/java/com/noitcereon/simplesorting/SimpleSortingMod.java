0d60e4b39efb783dd01d9f35640d26675524cd01
Thomas 'Noit' A <tba@live.dk>
2022-08-15 17:35:45 +0200

    Refactor: Change the way screenHandler casts
    This way was recommended to me by warjort (Adrian Brock)

--- src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
+++ src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
@@ -12,43 +12,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SimpleSortingMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(ModInfo.MOD_ID);
-	public static final Identifier INVENTORY_SORT_REQUEST_ID = new Identifier(ModInfo.MOD_ID, "inventory-sort-request");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(ModInfo.MOD_ID);
+    public static final Identifier INVENTORY_SORT_REQUEST_ID = new Identifier(ModInfo.MOD_ID, "inventory-sort-request");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		LOGGER.info("Initialising {}", ModInfo.MOD_ID);
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        LOGGER.info("Initialising {}", ModInfo.MOD_ID);
 
-		ServerPlayNetworking.registerGlobalReceiver(INVENTORY_SORT_REQUEST_ID, (server, player, handler, buf, responseSender) -> {
-			server.execute(() -> {
-				LOGGER.debug("Server received package: " + INVENTORY_SORT_REQUEST_ID.getNamespace() + " " + INVENTORY_SORT_REQUEST_ID.getPath());
+        ServerPlayNetworking.registerGlobalReceiver(INVENTORY_SORT_REQUEST_ID, (server, player, handler, buf, responseSender) -> {
+            server.execute(() -> {
+                LOGGER.debug("Server received package: " + INVENTORY_SORT_REQUEST_ID.getNamespace() + " " + INVENTORY_SORT_REQUEST_ID.getPath());
 
-				sortCurrentlyOpenInventory(player);
-			});
-		});
-	}
+                sortCurrentlyOpenInventory(player);
+            });
+        });
+    }
 
-	private void sortCurrentlyOpenInventory(ServerPlayerEntity player) {
-		try {
-			ScreenHandler screenHandler = player.currentScreenHandler;
-			if(screenHandler.getClass() != GenericContainerScreenHandler.class){
-				throw new IllegalArgumentException(screenHandler.getClass() + "is an invalid screen handler. Should be a GenericContainerScreenHandler.");
-			}
-			if(screenHandler.canUse(player)){
-				GenericContainerScreenHandler containerScreenHandler = (GenericContainerScreenHandler) screenHandler;
-				Inventory containerInventory = containerScreenHandler.getInventory();
-				InventorySorter.sortInventory(containerInventory);
-				containerInventory.markDirty();
-			}
-		}
-		catch(IllegalArgumentException exception){
-			LOGGER.warn(exception.getMessage());
-		}
-	}
+    private void sortCurrentlyOpenInventory(ServerPlayerEntity player) {
+        ScreenHandler screenHandler = player.currentScreenHandler;
+        if (screenHandler instanceof GenericContainerScreenHandler containerScreenHandler) {
+            if (!screenHandler.canUse(player)){
+                LOGGER.info("Failed to sort, because player cannot use the container anymore.");
+                return;
+            }
+
+            Inventory containerInventory = containerScreenHandler.getInventory();
+            InventorySorter.sortInventory(containerInventory);
+            containerInventory.markDirty();
+        }
+        else{
+            LOGGER.warn("player.currentScreenHandler returned {}. Simply Sorting uses GenericContainerScreenHandler to function.", screenHandler.getClass());
+        }
+    }
 }
