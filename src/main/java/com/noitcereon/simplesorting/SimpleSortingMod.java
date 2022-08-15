9de1f876c6f81a8bb58e1f10ccad5b53a9b43a2f
Thomas 'Noit' A <tba@live.dk>
2022-08-15 15:18:09 +0200

    Fix: use ModInfo util class in LOGGER
    Replaced hardcoded mod id.

--- src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
+++ src/main/java/com/noitcereon/simplesorting/SimpleSortingMod.java
@@ -22,7 +22,7 @@ public class SimpleSortingMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("simplesorting");
+	public static final Logger LOGGER = LoggerFactory.getLogger(ModInfo.MOD_ID);
 	public static final Identifier INVENTORY_SORT_REQUEST_ID = new Identifier(ModInfo.MOD_ID, "inventory-sort-request");
 
 	@Override
