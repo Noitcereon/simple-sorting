9fac6751e7b6ed7e80dbbefb5284ee8c5464d9bd
Thomas 'Noit' A <tba@live.dk>
2022-08-24 14:17:59 +0200

    Feat: Add Sort button to Shulkers + refactor btn creation

--- src/main/java/com/noitcereon/simplesorting/SimpleSortingModClient.java
+++ src/main/java/com/noitcereon/simplesorting/SimpleSortingModClient.java
@@ -6,9 +6,11 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 
 
 public class SimpleSortingModClient implements ClientModInitializer {
@@ -18,13 +20,23 @@ public class SimpleSortingModClient implements ClientModInitializer {
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             if (screen instanceof GenericContainerScreen) {
                 Screens.getButtons(screen).add(
-                        new ButtonWidget((scaledWidth / 100) * 70, (scaledHeight / 100) * 5, 40, 16, Text.literal("Sort"), (btn) -> {
-                            SimpleSortingMod.LOGGER.debug("Sort Button pressed");
-                            PacketByteBuf packet = PacketByteBufs.empty();
-                            ClientPlayNetworking.send(SimpleSortingMod.INVENTORY_SORT_REQUEST_ID, packet); // this triggers the global receiver registered with the same identifier.
-                        })
+                        createSortButton(scaledWidth, scaledHeight)
                 );
             }
+            if(screen instanceof ShulkerBoxScreen){
+                Screens.getButtons(screen).add(
+                        createSortButton(scaledWidth, scaledHeight)
+                );
+            }
+        });
+    }
+
+    @NotNull
+    private ButtonWidget createSortButton(int scaledWidth, int scaledHeight) {
+        return new ButtonWidget((scaledWidth / 100) * 70, (scaledHeight / 100) * 5, 40, 16, Text.literal("Sort"), (btn) -> {
+            SimpleSortingMod.LOGGER.debug("Sort Button pressed");
+            PacketByteBuf packet = PacketByteBufs.empty();
+            ClientPlayNetworking.send(SimpleSortingMod.INVENTORY_SORT_REQUEST_ID, packet); // this triggers the global receiver registered with the same identifier.
         });
     }
 }
