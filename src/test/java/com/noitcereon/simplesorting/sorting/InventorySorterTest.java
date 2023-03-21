1532cf3e6a8df5474f0a40da7cda60ed0c21de9b
Noitcereon <tba@live.dk>
2023-03-21 22:12:24 +0100

    Test(sorting): Add InventorySorterTest class

---
+++ src/test/java/com/noitcereon/simplesorting/sorting/InventorySorterTest.java
@@ -0,0 +1,12 @@
+package com.noitcereon.simplesorting.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class InventorySorterTest {
+
+    @Test
+    void sortInventory() {
+        fail("Not tested!");
+    }
+}
